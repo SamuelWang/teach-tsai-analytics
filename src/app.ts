0629a85197d6da8b538e9a7c9facc241106828f3
Samuel Wang <chieh0919@gmail.com>
2023-11-12 16:28:19 +0800

    add 404 error handler

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 import dotenv from 'dotenv';
 import express, { Express } from 'express';
+import createError from 'http-errors';
 import apiRouter from './routes/api';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
+
+app.use((req, res, next) => {
+  next(createError(404));
+});
 
 app.use('/api', apiRouter);
 
