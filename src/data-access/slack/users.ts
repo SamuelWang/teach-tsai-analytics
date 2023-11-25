d65a36cf16ba6860bf235a7ce8cb84e47565db42
Samuel Wang <chieh0919@gmail.com>
2023-11-25 11:20:48 +0800

    update the return value of the Users.list()

--- src/data-access/slack/users.ts
+++ src/data-access/slack/users.ts
@@ -1,13 +1,15 @@
+import { UsersListResponse } from '@slack/web-api';
 import { get as getClient } from './client';
 
-export async function list() {
+export async function list(): Promise<UsersListResponse> {
   const client = getClient();
 
   try {
     const result = await client.users.list();
 
-    return result.members;
+    return result;
   } catch (error) {
     console.error(error);
+    return Promise.resolve({ ok: false });
   }
 }
