fa46df70e18672c11606065fc814721ceb8a3919
Samuel Wang <chieh0919@gmail.com>
2023-11-25 11:26:36 +0800

    add the Slack data access of chat

---
+++ src/data-access/slack/chat.ts
@@ -0,0 +1,30 @@
+import { ChatPostMessageResponse } from '@slack/web-api';
+import { get as getClient } from './client';
+
+export async function postMessage(
+  channelId: string,
+  content: string[],
+): Promise<ChatPostMessageResponse> {
+  const client = getClient();
+
+  try {
+    const result = await client.chat.postMessage({
+      channel: channelId,
+      blocks: content.map((text) => ({
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text,
+        },
+      })),
+    });
+
+    return result;
+  } catch (error) {
+    console.error(error);
+    return Promise.resolve({
+      ok: false,
+      channel: channelId,
+    });
+  }
+}
--- src/data-access/slack/index.ts
+++ src/data-access/slack/index.ts
@@ -1,2 +1,3 @@
+export * as Chat from './chat';
 export * as Client from './client';
 export * as Users from './users';
