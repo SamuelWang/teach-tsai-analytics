b991b5336b1f4dd384e7c3ae30a0476039eae1c0
Samuel Wang <chieh0919@gmail.com>
2023-11-26 16:08:56 +0800

    add Conversions.replies() method of the Slack data access

--- src/data-access/slack/conversations.ts
+++ src/data-access/slack/conversations.ts
@@ -1,4 +1,7 @@
-import { ConversationsHistoryResponse } from '@slack/web-api';
+import {
+  ConversationsHistoryResponse,
+  ConversationsRepliesResponse,
+} from '@slack/web-api';
 import { get as getClient } from './client';
 
 export async function history(
@@ -13,6 +16,28 @@ export async function history(
       channel: channelId,
       oldest: String(start / 1000),
       latest: String(end / 1000),
+    });
+
+    return result;
+  } catch (error) {
+    console.error(error);
+    return Promise.resolve({
+      ok: false,
+      messages: [],
+    });
+  }
+}
+
+export async function replies(
+  ts: number,
+): Promise<ConversationsRepliesResponse> {
+  const client = getClient();
+  const channelId = process.env.CHANNEL_ID ?? '';
+
+  try {
+    const result = await client.conversations.replies({
+      channel: channelId,
+      ts: String(ts),
     });
 
     return result;
