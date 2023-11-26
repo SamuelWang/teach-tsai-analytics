0c0e950353a60c5a3cde8a0a64b0cc0d3b0b5c3b
Samuel Wang <chieh0919@gmail.com>
2023-11-26 16:02:15 +0800

    add Conversions.history() method of the Slack data access

---
+++ src/data-access/slack/conversations.ts
@@ -0,0 +1,26 @@
+import { ConversationsHistoryResponse } from '@slack/web-api';
+import { get as getClient } from './client';
+
+export async function history(
+  start: number,
+  end: number,
+): Promise<ConversationsHistoryResponse> {
+  const client = getClient();
+  const channelId = process.env.CHANNEL_ID ?? '';
+
+  try {
+    const result = await client.conversations.history({
+      channel: channelId,
+      oldest: String(start / 1000),
+      latest: String(end / 1000),
+    });
+
+    return result;
+  } catch (error) {
+    console.error(error);
+    return Promise.resolve({
+      ok: false,
+      messages: [],
+    });
+  }
+}
--- src/data-access/slack/index.ts
+++ src/data-access/slack/index.ts
@@ -1,3 +1,4 @@
 export * as Chat from './chat';
 export * as Client from './client';
+export * as Conversations from './conversations';
 export * as Users from './users';
