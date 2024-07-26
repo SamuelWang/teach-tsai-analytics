f152faa39b1287d2554bedfd1f40a9b4e8544de8
Samuel Wang <chieh0919@gmail.com>
2024-07-26 03:27:15 +0000

    add APIs

--- api/index.ts
+++ api/index.ts
@@ -1,12 +1,45 @@
 import dotenv from 'dotenv';
 import express, { Express, Request, Response } from 'express';
 import createError from 'http-errors';
+import * as DataAccess from '../data-access';
+import { analyseReplies } from '../services/analysis';
+import { generateContentFromMealGroups, getReplies } from '../services/message';
+import { startAnalysisJob } from '../services/job';
 
 dotenv.config();
 
 const app: Express = express();
 const hostname = process.env.HOST ?? 'localhost';
 const port = Number.parseInt(process.env.PORT ?? '3000', 10);
+
+app.get('/counting/post', async (req: Request, res: Response) => {
+  if (req.query['key'] !== process.env.ACCESS_KEY) {
+    res.sendStatus(401);
+    return;
+  }
+
+  const replies = await getReplies();
+  const groups = analyseReplies(replies);
+  const content = generateContentFromMealGroups(groups);
+  const channelId = process.env.COUNTER_CHANNEL_ID ?? '';
+  const postResult = await DataAccess.Slack.Chat.postMessage(
+    channelId,
+    content,
+  );
+
+  res.sendStatus(postResult.ok ? 204 : 500);
+});
+
+app.post('/cron-job', (req: Request, res: Response) => {
+  if (req.query['key'] !== process.env.ACCESS_KEY) {
+    res.sendStatus(401);
+    return;
+  }
+
+  startAnalysisJob();
+
+  res.sendStatus(201);
+});
 
 app.get('/', (_req: Request, res: Response) => {
   res.send("Hi, I'm Teacher Tsai Counter.");
